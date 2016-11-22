82d8aaae71330a66e143dd8a2ca7d6b297f9279f
Unknown <silagirgin@gmail.com>
2016-11-22 21:45:10 +0300

    dddd

---
+++ gitProject1/gitProject1/gitProject1.cpp
@@ -0,0 +1,16 @@
+// gitProject1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "sila" << endl;
+
+    return 0;
+}
+
